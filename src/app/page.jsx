e395196c8fe7b2d7d42cbdf5fd238deef314bc52
stivex001 <stephenadeyemo@gmail.com>
2023-08-06 13:51:46 +0100

    feat: integrated the tables with api datas

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,39 @@
 import UserTables from "@/components/UserTables";
 import Image from "next/image";
-import {AiOutlineSearch} from "react-icons/ai"
+import { AiOutlineSearch } from "react-icons/ai";
 
-export default function Home() {
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
-    <main className="py-5 px-6">
+    <main className="py-5 px-6 w-full md:w-[90%] lg:w-[80%]  md:mx-auto">
       <div className="shadow-md py-5 px-6 mx-auto">
         <div className="flex justify-between items-center">
           <p className="text-xl text-[rgb(23, 25, 35)]">Members</p>
           <div className="flex items-center gap-2 border px-5 py-2 rounded-lg">
             <AiOutlineSearch color="#566072" />
-            <input type="text" placeholder="Search" className="border-none outline-none focus:ring-cyan-400" />
+            <input
+              type="text"
+              placeholder="Search"
+              className="border-none outline-none focus:ring-cyan-400"
+            />
           </div>
         </div>
         {/* UserTables */}
-        <UserTables />
+
+        <UserTables data={data} />
       </div>
     </main>
   );
--- src/components/UserTables.jsx
+++ src/components/UserTables.jsx
@@ -6,7 +6,7 @@ import { AiOutlineArrowDown } from "react-icons/ai";
 import { FiEdit2 } from "react-icons/fi";
 import { RiDeleteBin5Line } from "react-icons/ri";
 
-const UserTables = () => {
+const UserTables = ({ data }) => {
   return (
     <div className="mt-5 relative w-full flex flex-col shadow-lg mb-6">
       <div className="block bg-transparent w-full overflow-x-auto ">
@@ -33,47 +33,52 @@ const UserTables = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className="border border-solid border-l-0 ">
-              <td className="text-sm px-6 py-3 flex gap-3 items-center">
-                <input
-                  type="checkbox"
-                  name=""
-                  id=""
-                  className="cursor-pointer"
-                />
-                <Image
-                  src=""
-                  alt=""
-                  className="w-7 h-7 rounded-full object-cover"
-                />
-                <div className="flex flex-col">
-                  <p className="text-sm text-[rgb(23, 25, 35)] truncate font-medium">
-                    Christian Nwamba
-                  </p>
-                  <span className="text-sm text-gray-700  truncate font-medium">
-                    @christian
+            {data.map((user) => (
+              <tr className="border border-solid border-l-0 " key={user.id}>
+                <td className="text-sm px-6 py-3 flex gap-3 items-center">
+                  <input
+                    type="checkbox"
+                    name=""
+                    id=""
+                    className="cursor-pointer"
+                  />
+                  <Image
+                    src=""
+                    alt=""
+                    className="w-7 h-7 rounded-full object-cover"
+                  />
+                  <div className="flex flex-col">
+                    <p className="text-sm text-[rgb(23, 25, 35)] truncate font-medium">
+                      {user.name}
+                    </p>
+                    <span className="text-sm text-gray-700  truncate font-medium">
+                      @{user.username}
+                    </span>
+                  </div>
+                </td>
+                <td className="text-md px-6 py-3">
+                  <span className=" py-[2px] px-2 bg-[#6cfcd5] text-sm font-bold rounded-sm text-[#22543d]">
+                    Active
                   </span>
-                </div>
-              </td>
-              <td className="text-md px-6 py-3">
-                <span className=" py-[2px] px-2 bg-[#6cfcd5] text-sm font-bold rounded-sm text-[#22543d]">
-                  Active
-                </span>
-              </td>
-              <td className="text-sm px-6 py-3 truncate text-[#4a5568]">
-                christian@chakra-ui.com
-              </td>
-              <td className="text-sm px-6 py-3 truncate text-[#4a5568]">
-                Senior Developer Advocate
-              </td>
-              <td className="text-sm px-6 py-3 truncate">*****</td>
-              <td className="text-sm px-6 py-3 truncate">
-                <div className="flex items-center gap-4">
-                  <RiDeleteBin5Line />
-                  <FiEdit2 />
-                </div>
-              </td>
-            </tr>
+                </td>
+                <td className="text-sm px-6 py-3 truncate text-[#4a5568]">
+                  {user.email}
+                </td>
+                <td className="text-sm px-6 py-3 truncate text-[#4a5568]">
+                  {user.company.name}
+                </td>
+                <td className="text-sm px-6 py-3 truncate">*****</td>
+                <td className="text-sm px-6 py-3 truncate">
+                  <div className="flex items-center gap-4 ">
+                    <RiDeleteBin5Line
+                      size={20}
+                      className="bg-white cursor-pointer"
+                    />
+                    <FiEdit2 size={20} className="bg-white cursor-pointer" />
+                  </div>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
